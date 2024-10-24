e885cd810cbafa45d2b944d290a2410805fff02b
QuocKhang <hakuingrease@gmail.com>
2024-10-24 23:47:03 +0700

    add led_toggle

---
+++ main/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_system.h"
+
+#define LED_GPIO GPIO_NUM_2
+
+void app_main(void){
+    gpio_set_direction(LED_GPIO,GPIO_MODE_OUTPUT);
+
+    while(1){
+        gpio_set_level(LED_GPIO,1);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        gpio_set_level(LED_GPIO,0);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+
+}
